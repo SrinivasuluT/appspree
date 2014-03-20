4d6f8e702b98c48f30ff42d526011da77b0c4bc0
SrinivasuluT <sri10586@gmail.com>
2014-03-20 08:06:54 -0700

    spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Appspree::Application.config.secret_key_base = '3a8af9b7380e33e2a28d8e1204947812adb258dccda1a061d782683026f38fb7b84d7843c5ac87355092258a1a8b6bd14c4f8c215176bd9df296d7fd471a862c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Appspree::Application.config.session_store :cookie_store, key: '_appspree_session'
