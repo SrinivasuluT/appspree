97ae363b4dfdcfbdfa88f9ddd8f2c55cf666a145
SrinivasuluT <sri10586@gmail.com>
2014-03-20 12:40:33 -0700

    spree

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,8 @@ Appspree::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-	config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+  config.assets.initialize_on_precompile = true
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
